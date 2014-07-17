ba5440d4a280e112eb0199d4d3d10ed6b63a3596
Jeannie Chirayus <iamjeannie@gmail.com>
2014-07-17 15:09:08 -0400

    views:index & _master created

---
+++ app/views/_master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Developer's Best Friend</title>
+</head>
+
+<body>
+
+	 @yield('content')
+
+</body>
+
+</html>
---
+++ app/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('_master')
+
+
+@section('content')
+
+
+	<h1>Developer's Best Friend:</h1>
+	<p>Explaination</p>
+
+@stop
